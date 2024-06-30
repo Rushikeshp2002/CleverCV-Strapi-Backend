ab7494c3fb157da6f94109e3b882ac3ab3d55b5d
Rushikesh Patil <rushikeshp7558@gmail.com>
2024-07-01 00:51:18 +0530

    Changed server.js file

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,16 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'script-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'your-frontend-url.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'your-frontend-url.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['https://clever-cv-ai-resume-builder.vercel.app'], // Remove trailing slash
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-    },
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  
 ];
+
+
+// https://clever-cv-ai-resume-builder.vercel.app/
--- config/server.js
+++ config/server.js
@@ -7,4 +7,10 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  settings: {
+    cors: {
+      origin: ['*'], //allow all origins
+      headers: ['*'], //allow all headers
+    },
+  },
 });
