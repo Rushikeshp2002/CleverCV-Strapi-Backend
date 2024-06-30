a0d354600855853f6a5c9cabf496a532b737e7ac
Rushikesh Patil <rushikeshp7558@gmail.com>
2024-06-30 20:59:19 +0530

    Changed some CORS configuration in middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,27 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'your-frontend-url.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'your-frontend-url.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://clever-cv-ai-resume-builder.vercel.app/'], // Replace with your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
@@ -10,3 +29,7 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+// https://clever-cv-ai-resume-builder.vercel.app/
